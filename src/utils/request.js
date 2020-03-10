e413b2ea6f83aec861bfe609743614d2f96e8ee5
MyNameIsLiDaDa <ljs2118@163.com>
2020-03-10 14:36:30 +0800

    commit test

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8007',
     timeout: 5000
 })
 
